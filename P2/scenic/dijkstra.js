da9bf62af36efe3d4a6844314aa517fbf8b7279f
Lajek20 <71812123+Lajek20@users.noreply.github.com>
2021-04-08 16:14:01 +0200

    Update dijkstra.js

--- P2/scenic/dijkstra.js
+++ P2/scenic/dijkstra.js
@@ -13,10 +13,14 @@ let shortestDistanceNode = (distances, visited) => {
 
     // for each node in the distances object
     for (let node in distances){
+        // if no node has been assigned to shortest yet 
+        // or if the current node's distance is smaller than current shortest 
         let currentIsShortest =
             shortest === null || distances[node] < distances[shortest];
-
+        
+        // and if the current node is in the unvisited set 
         if (currentIsShortest && !visited.includes(node)){
+            // update shortest to be the current node
             shortest = node;
         }
     }
@@ -24,38 +28,57 @@ let shortestDistanceNode = (distances, visited) => {
 };
 
 let findShortestPath = (graph, startNode, endNode) => {
+    
+    // track distances from the start node using a hash opbject 
     let distances = {};
     distances[endNode] = "Infinity";
     distances = Object.assign(distances, graph[startNode]);
-
+    
+    // track paths using a hash object 
     let parents = {endNode: null};
     for (let child in graph[startNode]){
         parents[child] = startNode;
     }
-
+    
+    // collect visited nodes
     let visited = [];
-
+    
+    // find the nearest node 
     let node = shortestDistanceNode(distances, visited);
 
     while (node){
+        // finde its distance from the start node and its child nodes
         let distance = distances[node];
         let children = graph[node];
 
         for (let child in children){
+            // make sure each child node is not the start node 
             if (String(child) === String(startNode)){
                 continue;
             } else {
+                // save the distance from the start node to the child node
+                let newdistance = distance + children[child];
+                
+                // if there's no recorded distance from the start node to the child node in the distances object
+                // or if the recorded distance is shorter than the previously stored distance from the start node to the child node
                 let newdistance = distance + children[child];
                 if (!distances[child] || distances[child] > newdistance){
+                    // save the distance to the object 
                     distances[child] = newdistance;
+                    //record the path
                     parents[child] = node;
                 }
             }
         }
+        // move the current node to the visited set
         visited.push(node);
-
+        
+        //move to the nearest neighor node
         node = shortestDistanceNode(distances, visited);
     }
+    
+    // using the stored paths from start node to end node 
+    // record the shortest path
     let shortestPath = [endNode];
     let parent = parents[endNode];
     while (parent){
@@ -63,11 +86,13 @@ let findShortestPath = (graph, startNode, endNode) => {
         parent = parents[parent];
     }
     shortestPath.reverse();
-
+    
+    // this is the shortedt path
     let results = {
         distance: distances[endNode],
         path: shortestPath,
     };
+    // return the shortest path and the end node's distance from the start node 
     return results;
 };
 

933be02deae47b1e344e8123bccc4e220031d3aa
hijabnasir <73532018+hijabnasir@users.noreply.github.com>
2021-04-17 16:52:31 +0200

    Add files via upload

---
+++ P2/scenic/koordinater.js
@@ -0,0 +1,129 @@
+//koordinaterne til ruterne
+
+//gul koordinaterne
+let Rx = {
+	kryds:"HansOlriksVej_JohanKellersVej",
+	bg:"?",
+	lg:"?",
+	ruter:"G1, G3"
+}
+
+let G1 = {
+    kryds:"Enghavevej_HansOlriksVej",
+	bg:"55.657461",
+	lg:"12.538591",
+	ruter:"G2, Rx"
+}
+
+let G2 = {
+	kryds:"Enghavevej_EdvardStormsVej",
+	bg:"55.656756",
+	lg:"12.538172",
+	ruter:"G1, G3, G6"
+}
+
+let G3 = {
+	kryds:"JohanKellersVej_EdvardStormsVej",
+	bg:"55.656960",
+	lg:"12.537041",
+	ruter:""
+}
+
+let G4 = {
+	kryds:"JohanKellersVej_Tranehavevej",
+	bg:"55.656082",
+	lg:"12.536541",
+	ruter:""
+}
+
+let G5 = {
+	kryds:"ErnestKapersVej_Tranehavevej",
+	bg:"55.655559",
+	lg:"12.537381",
+	ruter:""
+}
+
+let G6 = {
+	kryds:"Enghavevej_Tranehavevej",
+	bg:"55.655559",
+	lg:"12.537854",
+	ruter:""
+}
+
+let G7 = {
+	kryds:"Vesterkirkegaard_HVRolstedsVej",
+	bg:"55.655486",
+	lg:"12.534921",
+	ruter:""
+}
+
+let G8 = {
+	kryds:"Vesterkirkegaard_JohanKellersVej",
+	bg:"55.654365",
+	lg:"12.535568",
+	ruter:""
+}
+
+let G9 = {
+	kryds:"Vesterkirkegaard_NatalieZahlesVej",
+	bg:"55.656412",
+	lg:"12.534380",
+	ruter:""
+}
+
+//rød koordinaterne
+
+let H1 = {
+    kryds: "HansOlrikVej_JohanKellersVej", 
+    bg: "55.657667", 
+    lg: "12.537472", 
+    ruter: ""
+}
+
+let H2 = {
+    kryds: "Tranehavevej_h.vRolstedsVej",
+    bg:"55.656667", 
+    lg:"12.535750",
+    ruter: ""
+}
+
+let H3 = {
+    kryds: "Tranehavevej_Tranehavegård",
+    bg:"55.657528",
+    lg:"12.534528",
+    ruter: ""
+}
+
+let H4 = {
+    kryds: "ErnstKapersVej_ErnstKapersVej",
+    bg:"55.653417",
+    lg:"12.533722",
+    ruter: ""
+}
+
+let H5 = {
+    kryds: "ErnstKapersVej_ErnstKapersVej",
+    bg:"55.653722",
+    lg:"12.534944",
+    ruter: ""
+}
+
+let H6 = {
+    kryds: "EdwardStormsVej_H.V.RolstedsVej",
+    bg:"55.657111",
+    lg:"12.536083",
+    ruter: ""
+}
+
+
+//grøn koordinaterne
+let G1 = {
+    kryds: "Trandhavevej_Tranehavegård",
+    bg:"55.657273",
+    lg:"12.534964",
+    ruter: ""
+}
+
+
+
+
